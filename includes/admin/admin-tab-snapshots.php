3840dbf25b0b2e4877d7d343f15a9a6181701bf7
Jon <cairocoder01@gmail.com>
2025-09-24 12:22:33 +0300

    Update fields and build snapshots

--- includes/admin/admin-tab-snapshots.php
+++ includes/admin/admin-tab-snapshots.php
@@ -35,78 +35,95 @@ class DT_Data_Reporting_Tab_Snapshots
     }
 
     public function main_column() {
-        $limit = 100;
-        $offset = 0;
-        if ( isset( $_GET["limit"] ) ) {
-            $limit = sanitize_key( wp_unslash( $_GET["limit"] ) );
-        }
-        if ( isset( $_GET["offset"] ) ) {
-            $offset = sanitize_key( wp_unslash( $_GET["offset"] ) );
-        }
+      global $wpdb;
 
-        $this->settings_selection();
+      $this->settings_selection();
 
-        if ( isset( $_GET["button"] ) && $_GET["button"] === "run" ) {
-          DT_Data_Reporting_Snapshot_Tools::run_snapshot_task();
-        } else
+      if ( isset( $_GET["button"] ) && $_GET["button"] === "run" ) {
+        DT_Data_Reporting_Snapshot_Tools::run_snapshot_task();
+      }
 
-        if ( isset( $_GET["type"] ) && isset( $_GET["period"] ) ) {
-          $type = sanitize_key( wp_unslash( $_GET["type"] ) );
-          $period = sanitize_key( wp_unslash( $_GET["period"] ) );
-          $post_id = isset($_GET["post_id"]) ? (int)sanitize_key(wp_unslash($_GET["post_id"])) : null;
-//          $snapshots = DT_Data_Reporting_Tools::get_snapshots( $type, $post_id, $period );
-          [$columns, $rows, $total] = DT_Data_Reporting_Tools::get_snapshots( $type, $post_id, $period );
-//          [$columns, $rows, $total] = DT_Data_Reporting_Tools::get_data($this->type, $this->config_key, false, $limit, $offset);
+      $columns = [['name' => 'Interval']];
 
-//          $columns = ['ID', 'group_status', 'members', 'member_count', 'post_date'];
-//          $total = 0;
-          $this->main_column_table( $columns, $rows, $total );
-        }
+      $select = "select period_interval";
+      $where = "where period_interval is not null";
+      $group_by = "group by period_interval";
+      $order_by = "";
+
+      if ( isset( $_GET["type"] ) && !empty( $_GET["type"] ) ) {
+        $select .= ", post_type";
+        $where .= " and post_type = '" . sanitize_key( wp_unslash( $_GET["type"] ) ) . "'";
+        $group_by .= ", post_type";
+        $columns[] = ['name' => 'Post Type'];
+      }
+      if ( isset( $_GET["interval"] ) && !empty( $_GET["interval"] ) ) {
+        $select .= ", period";
+        $where .= " and period_interval = '" . sanitize_key( wp_unslash( $_GET["interval"] ) ) . "'";
+        $group_by .= ", period";
+        $columns[] = ['name' => 'Period'];
+        $order_by = "ORDER BY period DESC";
+      } else {
+        $select .= ", max(period_start) as max_start, max(period_end) as max_end";
+        $columns[] = ['name' => 'Last Period Start'];
+        $columns[] = ['name' => 'Last Period End'];
+      }
+
+      $select .= ", count(*) as total";
+      $columns[] = ['name' => 'Total'];
+
+      $table_name = $wpdb->prefix . "dt_post_snapshots";
+      $query = "$select from $table_name $where $group_by $order_by";
+      dt_write_log($query);
+      $results = $wpdb->get_results( $query, ARRAY_A );
+
+      $this->main_column_table( $columns, $results, count( $results ) );
     }
 
     public function settings_selection() {
       $post_types = DT_Posts::get_post_types();
       ?>
-      <p>Please select a type and period to view snapshots.</p>
+      <p>Please select a type and/or period to view snapshot status.</p>
 
       <form method="GET">
         <input type="hidden" name="page" value="disciple-tools-data-reporting">
         <input type="hidden" name="tab" value="snapshots">
         <div style="display: flex; flex-direction: row; gap: 1rem;">
-        <div>
-          <h3>Record Type</h3>
-          <?php foreach ($post_types as $post_type) : ?>
-            <?php
-            $post_label = DT_Posts::get_label_for_post_type( $post_type );
-            ?>
-            <input type="radio" id="type_<?php echo esc_attr($post_type); ?>"
-                   name="type" value="<?php echo esc_attr($post_type); ?>"
-              <?php checked(isset($_GET['type']) && $_GET['type'] === $post_type); ?>>
-            <label
-              for="type_<?php echo esc_attr($post_type); ?>"><?php echo esc_html( $post_label ); ?></label>
-            <br>
-          <?php endforeach; ?>
-        </div>
+          <div>
+            <h3>Record Type</h3>
+            <input type="radio" id="type_none" name="type" value=""
+              <?php checked(!isset($_GET['type']) || $_GET['type'] === ''); ?>>
+            <label for="type_none">All</label><br>
 
-        <div>
-          <h3>Time Period</h3>
-          <input type="radio" id="period_month" name="period" value="month"
-            <?php checked(isset($_GET['period']) && $_GET['period'] === 'month'); ?>>
-          <label for="period_month">Month</label><br>
-
-          <input type="radio" id="period_quarter" name="period" value="quarter"
-            <?php checked(isset($_GET['period']) && $_GET['period'] === 'quarter'); ?>>
-          <label for="period_quarter">Quarter</label><br>
-
-          <input type="radio" id="period_year" name="period" value="year"
-            <?php checked(isset($_GET['period']) && $_GET['period'] === 'year'); ?>>
-          <label for="period_year">Year</label><br>
-        </div>
+            <?php foreach ($post_types as $post_type) : ?>
+              <?php
+              $post_label = DT_Posts::get_label_for_post_type( $post_type );
+              ?>
+              <input type="radio" id="type_<?php echo esc_attr($post_type); ?>"
+                     name="type" value="<?php echo esc_attr($post_type); ?>"
+                <?php checked(isset($_GET['type']) && $_GET['type'] === $post_type); ?>>
+              <label
+                for="type_<?php echo esc_attr($post_type); ?>"><?php echo esc_html( $post_label ); ?></label>
+              <br>
+            <?php endforeach; ?>
+          </div>
 
           <div>
-            <h3>Post ID</h3>
-            <label for="post_id" style="display:block;">Enter a post ID to view snapshots for a specific post.</label>
-            <input id="post_id" type="text" name="post_id" value="<?php echo isset($_GET['post_id']) ? esc_attr($_GET['post_id']) : ''; ?>">
+            <h3>Interval</h3>
+            <input type="radio" id="interval_none" name="interval" value=""
+              <?php checked(!isset($_GET['interval']) || $_GET['interval'] === ''); ?>>
+            <label for="interval_none">All</label><br>
+
+            <input type="radio" id="interval_month" name="interval" value="month"
+              <?php checked(isset($_GET['interval']) && $_GET['interval'] === 'month'); ?>>
+            <label for="interval_month">Month</label><br>
+
+            <input type="radio" id="interval_quarter" name="interval" value="quarter"
+              <?php checked(isset($_GET['interval']) && $_GET['interval'] === 'quarter'); ?>>
+            <label for="interval_quarter">Quarter</label><br>
+
+            <input type="radio" id="interval_year" name="interval" value="year"
+              <?php checked(isset($_GET['interval']) && $_GET['interval'] === 'year'); ?>>
+            <label for="interval_year">Year</label><br>
           </div>
 
         </div>
--- includes/snapshot-tools.php
+++ includes/snapshot-tools.php
@@ -1,6 +1,8 @@
 <?php
 
-
+/**
+ * Class DT_Data_Reporting_Snapshot_Tools
+ */
 class DT_Data_Reporting_Snapshot_Tools
 {
   public static $table_name = 'dt_post_snapshots';
@@ -11,7 +13,10 @@ class DT_Data_Reporting_Snapshot_Tools
 
   public static function run_snapshot_task()
   {
-    self::check_migrations();
+    $migration_success = self::check_migrations();
+    if ( !$migration_success ) {
+      return;
+    }
 
     $period = self::get_next_period();
 
@@ -63,18 +68,23 @@ class DT_Data_Reporting_Snapshot_Tools
             `period_interval` varchar(20) NOT NULL,
             `post_content` json NOT NULL,
             `snapshot_date` datetime NOT NULL,
-            PRIMARY KEY  (id),
+            UNIQUE KEY `unique_post_period` (`post_id`, `period`),
+            KEY `id` (`id`),
             KEY `post_id` (`post_id`),
             KEY `post_type` (`post_type`),
             KEY `period` (`period`),
             KEY `period_interval` (`period_interval`)
         ) $charset_collate;";
 
-      $wpdb->query( $sql );
+      $result = $wpdb->query($sql);
+      if ($result === false) {
+        dt_write_log('Failed to create table: ' . $table_name);
+        return false;
+      }
       dt_write_log('Created table: ' . $table_name);
     }
 
-    return $table_exists;
+    return true;
   }
 
   /**
@@ -120,18 +130,12 @@ class DT_Data_Reporting_Snapshot_Tools
       }
 
       // Check if the next expected period is in the past by adding the interval duration
-      $interval_duration = new DateInterval(match ($interval) {
-        self::INTERVAL_YEAR => 'P1Y',
-        self::INTERVAL_QUARTER => 'P3M',
-        self::INTERVAL_MONTH => 'P1M',
-      });
-
-      $next_expected = clone $period_end;
-      $next_expected->add($interval_duration);
+      $next_expected = self::alter_date_by_interval( $period_end, $interval );
+      dt_write_log( 'Next expected: ' . $next_expected->format('Y-m-d H:i:s') );
 
       if ($next_expected < $now) {
         // Found an interval that needs updating
-        return self::get_period_by_date($interval, $period_end);
+        return self::get_period_by_date($interval, $next_expected);
       }
     }
 
@@ -191,39 +195,133 @@ class DT_Data_Reporting_Snapshot_Tools
     ];
   }
 
+  /**
+   * Modify date by interval and multipler to add/remove year/quarter/month
+   * @param DateTime $date
+   * @param string $interval
+   * @param int $multiplier
+   * @return DateTime
+   * @throws DateMalformedStringException
+   */
+  private static function alter_date_by_interval( DateTime $date, string $interval, int $multiplier = 1 )
+  {
+    $alter_string = '';
+    switch ($interval) {
+      case self::INTERVAL_YEAR:
+        $alter_string = $multiplier . ' years';
+        break;
+      case self::INTERVAL_QUARTER:
+        $alter_string = ($multiplier * 3) . ' months';
+        break;
+      case self::INTERVAL_MONTH:
+        $alter_string = $multiplier . ' months';
+        break;
+    }
+
+    $altered_date = clone $date;
+
+    // when jumping months where the starting point is 31st day, we need to adjust the date.
+    // because of February with 28 days, we go back 3 days from 31 and adjust end of month below
+    $is_31 = $date->format('d') > 30;
+    if ($is_31) {
+      $altered_date->modify('-3 day');
+    }
+
+    $altered_date->modify( $alter_string );
+
+    if ( $is_31 ) {
+      $date->modify('last day of this month');
+    }
+
+    return $altered_date;
+  }
+
+  /**
+   * Builds snapshots for the provided period.
+   * @param array $period
+   * @return void
+   */
   private static function build_period_snapshots(array $period)
   {
     $field_settings_by_type = [];
+    $dt_post_types = DT_Posts::get_post_types();
 
     $activity_logs = self::get_activity_logs( $period['start'], $period['end'] );
     dt_write_log( "Activity logs for {$period['name']}: " . sizeof( $activity_logs ) );
 
-    $previous_period_date = new DateTime(); // $period['start'] - $period['interval']
+    $previous_period_date = self::alter_date_by_interval( $period['start'], $period['interval'], -1 );
     $previous_period = self::get_period_by_date( $period['interval'], $previous_period_date );
 
     $previous_snapshots = self::get_snapshots( $previous_period['name'] );
 
-    $snapshots = [];
+    // initialize snapshots with previous period data
+    $snapshots = array_map( function( $snapshot ) use ($period) {
+      $snapshot['period'] = $period['name'];
+      $snapshot['period_start'] = $period['start'];
+      $snapshot['period_end'] = $period['end'];
+      $snapshot['period_interval'] = $period['interval'];
+      return $snapshot;
+    }, $previous_snapshots);
+    dt_write_log( "Previous snapshots for {$period['name']}: " . sizeof( $previous_snapshots ) );
+
     // loop over activity logs
     if ( isset( $activity_logs ) && !empty( $activity_logs) ) {
       foreach ($activity_logs as $activity_log) {
         $post_type = $activity_log['object_type'];
         $post_id = $activity_log['object_id'];
-        $snapshot = $snapshots[$post_id] ?? ['ID' => $post_id, 'post_type' => $post_type];
 
+        // skip if post type is not in dt_post_types (e.g. attachments)
+        if ( !in_array( $post_type, $dt_post_types ) ) {
+          continue;
+        }
+
+        // get previous/existing snapshot or create new
+        $snapshot = $snapshots[$post_id] ?? [
+          'post_id' => $post_id,
+          'post_type' => $post_type,
+          'period' => $period['name'],
+          'period_start' => $period['start'],
+          'period_end' => $period['end'],
+          'period_interval' => $period['interval'],
+          'post_content' => [
+            'ID' => $post_id,
+            'post_type' => $post_type,
+          ]
+        ];
+
+        // make sure we have the field settings for this post type
         if ( !isset( $field_settings_by_type[$post_type] ) ) {
           $field_settings_by_type[$post_type] = DT_Posts::get_post_field_settings( $post_type );
         }
 
-        self::update_snapshot_value($snapshot, $field_settings_by_type[$post_type], $activity_log);
+        $post = $snapshot['post_content'] ?? [];
+        // update snapshot value based on field type
+        self::update_snapshot_value($post, $field_settings_by_type[$post_type], $activity_log);
+
+        $snapshot['post_content'] = $post;
+
+        // set post_title
+        if ( !isset( $snapshot['post_title'] ) ) {
+          $snapshot['post_title'] = $activity_log['object_name'];
+        }
+        if ( !isset( $snapshot['post_content']['name'] ) ) {
+          $snapshot['post_content']['name'] = $activity_log['object_name'];
+        }
 
         $snapshots[$post_id] = $snapshot;
       }
     }
+    dt_write_log( "Snapshots for {$period['name']}: " . sizeof( $snapshots ) );
 
     self::save_snapshots( $snapshots );
   }
 
+  /**
+   * Retrieves activity logs between the provided start and end dates.
+   * @param DateTime $start
+   * @param DateTime $end
+   * @return array|object|stdClass[]|null
+   */
   private static function get_activity_logs(DateTime $start, DateTime $end)
   {
     global $wpdb;
@@ -241,18 +339,233 @@ class DT_Data_Reporting_Snapshot_Tools
     return $wpdb->get_results($sql, ARRAY_A);
   }
 
+  /**
+   * Retrieves snapshots for a given period.
+   * @param mixed $name
+   * @return array
+   */
   private static function get_snapshots(mixed $name)
   {
-    // get by name and index by post_id
+    global $wpdb;
+    $table_name = $wpdb->prefix . self::$table_name;
+
+    dt_write_log( 'Getting snapshots for period: ' . $name );
+    $snapshots = $wpdb->get_results(
+      $wpdb->prepare(
+        "SELECT * FROM $table_name WHERE period = %s",
+        $name
+      ),
+      ARRAY_A
+    );
+    dt_write_log( 'Found snapshots: ' . sizeof( $snapshots ) );
+
+    // Index snapshots by post_id
+    $indexed_snapshots = [];
+    if ($snapshots) {
+      foreach ($snapshots as $snapshot) {
+        // Decode post_content if it's a JSON string
+        if ( is_string( $snapshot['post_content'] ) ) {
+          try {
+            $snapshot['post_content'] = json_decode( $snapshot['post_content'], true );
+          } catch (Exception $e) {
+            dt_write_log( "Error decoding JSON: " . $e->getMessage() );
+          }
+        }
+
+        // index snapshots by post_id
+        $indexed_snapshots[$snapshot['post_id']] = $snapshot;
+      }
+    }
+    dt_write_log( 'Indexed snapshots: ' . sizeof( $indexed_snapshots ) );
+
+    return $indexed_snapshots;
   }
 
-  private static function update_snapshot_value(&$snapshot, mixed $post_type, $activity_log)
+  /**
+   * Updates a snapshot value based on the provided activity.
+   * @param $snapshot
+   * @param mixed $field_settings
+   * @param $activity
+   * @return void
+   */
+  private static function update_snapshot_value(&$post, mixed $field_settings, $activity)
   {
-    // based on field_type, update snapshot value
+    $action = $activity['action'];
+    $field_type = $activity['field_type'];
+    $meta_key = $activity['meta_key'];
+    $meta_value = $activity['meta_value'];
+
+    $actions = [ 'field_update', 'connected to', 'disconnected from ' ];
+    if ( in_array( $action, $actions ) ) {
+      if ( !is_array( $post ) ) {
+        dt_write_log( 'Post is not an array: ' . json_encode( $post ) );
+      }
+      if ( !isset( $post['post_date'] ) ) {
+        $post['post_date'] = $activity['hist_time'];
+      }
+      switch ($field_type) {
+        case 'connection':
+          $field_setting = DT_Posts::get_post_field_settings_by_p2p($field_settings, $meta_key, ($action == 'disconnected from') ? ['from', 'to', 'any'] : ['to', 'from', 'any']);
+          if (!empty($field_setting)) {
+            $meta_key = $field_setting['key'];
+          }
+          // fall through to normal multi-value field update
+        case 'link':
+        case 'tags':
+        case 'location':
+        case 'location_meta':
+        case 'multi_select':
+          if ( !isset( $post[$meta_key] ) ) {
+            $post[$meta_key] = [];
+          } else if ( !is_array( $post[$meta_key] ) ) {
+            dt_write_log( "Post key is not an array: $meta_key | {$post['ID']}" );
+            dt_write_log( json_encode( $post ) );
+          }
+
+          if ( $meta_value === 'value_deleted' ) {
+            $old_value = $activity['old_value'];
+            $post[$meta_key] = array_values(array_filter($post[$meta_key], function ($value) use ($old_value) {
+              return $value !== $old_value;
+            }));
+          } else {
+            $post[$meta_key][] = $meta_value;
+          }
+          break;
+        case 'user_select':
+          if ( $meta_value === 'value_deleted') {
+            $post[$meta_key] = null;
+          } else {
+            $meta_array = explode( '-', $meta_value ); // Separate the type and id
+            $type = $meta_array[0]; // Build variables
+            if ( isset( $meta_array[1] ) ) {
+              $id = $meta_array[1];
+              if ($type == 'user' && $id) {
+                $post[$meta_key] = $id;
+              }
+            }
+          }
+          break;
+        case 'communication_channel':
+        case '':
+          // some communication channel activity don't have a field_type for some reason.
+          // The meta key is in the format contact_address_89a.
+          // The actual field key is sometimes in object_subtype, but sometimes that also has a suffix.
+          // So we'll just look for contact_* in the meta key and store it as an array field.
+          $meta_key_parts = explode('_', $meta_key);
+          if ( count($meta_key_parts) > 1 && $meta_key_parts[0] == 'contact' ) {
+            // only process contact_* fields
+            $meta_key = implode('_', [$meta_key_parts[0], $meta_key_parts[1]]);
+
+            // Use the same logic as for multi-value fields above
+            if (!isset($post[$meta_key])) {
+              $post[$meta_key] = [];
+            }
+
+            if ($meta_value === 'value_deleted') {
+              $old_value = $activity['old_value'];
+              $post[$meta_key] = array_values(array_filter($post[$meta_key], function ($value) use ($old_value) {
+                return $value !== $old_value;
+              }));
+            } else {
+              $post[$meta_key][] = $meta_value;
+            }
+          }
+          break;
+        case 'details': // communication_channel fields add a details record that we don't need
+        case 'hash': // magic links create hash fields that we don't need
+          break;
+        case 'key_select':
+        case 'date':
+        case 'datetime':
+        case 'number':
+        case 'boolean':
+        case 'text':
+        case 'textarea':
+        default:
+          if ( $meta_value === 'value_deleted') {
+            $post[$meta_key] = null;
+          } else {
+            $post[$meta_key] = $meta_value;
+          }
+          break;
+      }
+    } else if ( $action == 'deleted' ) {
+      $post['deleted'] = true;
+    }
   }
 
+  /**
+   * Saves provided snapshots to the dt_posts_snapshots database table.
+   * Filters out deleted posts before saving and ensures snapshots have valid required fields.
+   *
+   * @param array $snapshots An array of snapshot data to be saved. Each snapshot must include:
+   *                         - 'post_id': The ID of the post.
+   *                         - 'post_type': The type of the post.
+   *                         - 'period': The period associated with the snapshot.
+   *                         - 'period_start': The start period as a DateTime object.
+   *                         - 'period_end': The end period as a DateTime object.
+   *                         - 'period_interval': The interval of the period.
+   *                         Optionally includes:
+   *                         - 'post_title': The title of the post.
+   *                         - 'post_content': An array of content data for the post.
+   *
+   * @return void
+   */
   private static function save_snapshots(array $snapshots)
   {
     // save snapshots to dt_posts_snapshots
+    if ( !empty( $snapshots ) ) {
+      // remove deleted posts from snapshots before saving. We don't need future snapshots for deleted posts.
+      $filtered_snapshots = array_filter( $snapshots, function( $snapshot ) {
+        return !isset( $snapshot['post_content']['deleted'] );
+      });
+      dt_write_log( 'Saving snapshots: ' . sizeof( $filtered_snapshots ) );
+
+      global $wpdb;
+      $table_name = $wpdb->prefix . self::$table_name;
+      $now = (new DateTime())->format('Y-m-d H:i:s');
+
+      $logged_data = false;
+      foreach ($filtered_snapshots as $snapshot) {
+        if (empty($snapshot['post_id']) || empty($snapshot['post_type'])
+          || empty($snapshot['period']) || empty($snapshot['period_start'])
+          || empty($snapshot['period_end']) || empty($snapshot['period_interval'])) {
+          continue;
+        }
+
+        $data = [
+          'post_id' => $snapshot['post_id'],
+          'post_type' => $snapshot['post_type'],
+          'post_title' => $snapshot['post_title'] ?? '',
+          'period' => $snapshot['period'],
+          'period_start' => $snapshot['period_start']->format('Y-m-d H:i:s'),
+          'period_end' => $snapshot['period_end']->format('Y-m-d H:i:s'),
+          'period_interval' => $snapshot['period_interval'],
+          'post_content' => wp_json_encode($snapshot['post_content'] ?? []),
+          'snapshot_date' => $now
+        ];
+        if (!$logged_data) {
+//          dt_write_log( 'Saving data: ' . json_encode( $data ) );
+          $logged_data = true;
+        }
+        $format = [
+          '%d',
+          '%s',
+          '%s',
+          '%s',
+          '%s',
+          '%s',
+          '%s',
+          '%s',
+          '%s'
+        ];
+
+        $wpdb->replace(
+          $table_name,
+          $data,
+          $format
+        );
+      }
+    }
   }
 }
